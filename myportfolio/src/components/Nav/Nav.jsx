211e2926c7e026ecc58501157c32bc8ab9bb2b05
Trip25 <t.gurung1995@gmail.com>
2023-05-15 23:53:42 +0100

    working on the nav bar- need to look for icons

--- myportfolio/src/components/Nav/Nav.jsx
+++ myportfolio/src/components/Nav/Nav.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './nav.css'
+import {BiHomeHeart} from 'react-icons/bi'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className="nav">
+      <a href="#home" className="nav__link"><BiHomeHeart/>Home</a>
+      <a href="#about" className="nav__link"><BiHomeHeart/>About Me</a>
+      <a href="#experience" className="nav__link"><BiHomeHeart/>Experience</a>
+      <a href="#project" className="nav__link"><BiHomeHeart/>Projects</a>
+      <a href="#contact" className="nav__link"><BiHomeHeart/>Contact</a>
+    </nav>
   )
 }
 
