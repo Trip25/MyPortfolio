55e86355d5d71fd5bdd771091831cae1a9891c2b
Trip25 <t.gurung1995@gmail.com>
2023-05-29 23:34:56 +0100

    idk what i changed

--- myportfolio/src/components/Experience/Experience.jsx
+++ myportfolio/src/components/Experience/Experience.jsx
@@ -23,10 +23,11 @@ const Experience = () => {
     <h2>My Experience</h2>
 
     <div className='container experience__container'>
+      {/* Starting the front end */}
       <div className='experience__frontend'>
-        <h3>Frontend Development</h3>
         <div className='experience__content'>
         <article className='experience__article'>
+        <h3>Front-end Development</h3>
           <div className='logo-groups'>
             <img src={HTML} alt="html logo" id="html-logo" className='logos'/>
             <h4>HTML</h4>
@@ -41,33 +42,36 @@ const Experience = () => {
             <h4>JavaScript</h4>
           </div>
 
-         
           
           <div className='logo-groups'>
-          <img src={ReactLogo} alt="react logo" className='logos' id="react-logo" />
+            <img src={ReactLogo} alt="react logo" className='logos' id="react-logo" />
             <h4>React</h4>
           </div>
           
           <div className='logo-groups'>
-          <img src={Typescript} alt="typescript logo" className='logos' id="typescript-logo" />
-          <h4>TypeScript</h4>
+            <img src={Typescript} alt="typescript logo" className='logos' id="typescript-logo" />
+            <h4>TypeScript</h4>
           </div>
         </article>
-        </div>
+        </div> 
+        {/* closes the experience__content */}
       </div>
+      {/* closes the experience__frontend */}
 
       {/* Starting the back end */}
-      <div className='experience__backend'>
-        <h3 >Backend Development</h3>
-        <div className='experience__content'>
+    <div className='experience__backend'>
+      <div className='experience__content'>
+          
         <article className='experience__article'>
-        {/* To ADD 
-          SQL databases  */}
-          <div className='logo-groups'>
-          <img src={SQL} alt="sql logo" id="sql-logo" className='logos'/>
-          <h4>SQL</h4></div>
-          <div className='logo-groups'>
-          <img src={PostGreSQL} alt="postgresql logo" id="postgresql-logo" className='logos'/>
+        <h3 >Back-end Development</h3>
+          {/* SQL */}
+            <div className='logo-groups'>
+            <img src={SQL} alt="sql logo" id="sql-logo" className='logos'/>
+            <h4>SQL</h4>
+            </div>
+          {/* PostGreSQL */}
+            <div className='logo-groups'>
+            <img src={PostGreSQL} alt="postgresql logo" id="postgresql-logo" className='logos'/>
           <h4>PostGreSQL</h4>
           </div>
           
@@ -87,30 +91,35 @@ const Experience = () => {
           
           
         </article>
-        </div>
       </div>
+    </div>
+
+      {/* Starting the other skills */}
       <div className='experience__other'>
-        <h3>Other</h3>
+        
         <div className='experience__content'>
         <article className='experience__article'>
+        <h3>Other</h3>
+
         <div className='logo-groups'>
           <h4>User Experience/ User Interface</h4>
           </div>
+          {/* Git */}
           <div className='logo-groups'>
           <img src={Git} alt="git logo" id="git-logo" className='logos'/>
           <h4>Git</h4>
           </div>
-          
+          {/* Github */}
           <div className='logo-groups'>
           <img src={Github} alt="github logo" id="github-logo" className='logos'/>
           <h4>Github</h4>
           </div>
-          
+          {/* jest */}
           <div className='logo-groups'>
           <img src={jest} alt="jest logo" id="jest-logo" className='logos'/>
           <h4>Jest</h4>
           </div>
-          
+          {/* playwright */}
           <div className='logo-groups'>
           <img src={playwright} alt="playwright logo" id="playwright-logo" className='logos'/>
           <h4>Playwright End-to-end Testing</h4>
